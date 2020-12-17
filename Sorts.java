ce6a593c892de16ccbe489dcb02b4dd2308c2252
iamchwill <wchen20@stuy.edu>
2020-12-17 14:19:13 -0500

    Made selectionSort() Method

--- Sorts.java
+++ Sorts.java
@@ -1,4 +1,5 @@
 public class Sorts{
+
   public static void bubbleSort(int[] data){
     for (int i = data.length - 1; i > 0; i--){
       boolean done = true;
@@ -11,6 +12,18 @@ public class Sorts{
         }
       }
       if (done) i = 0;
+    }
+  }
+
+  public static void selectionSort(int[] data){
+    for (int i = 0; i < data.length; i++){
+      int smallest = i;
+      for (int j = i; j < data.length; j++){
+        if(data[j] <= data[smallest]) smallest = j;
+      }
+      int temp = data[i];
+      data[i] = data[smallest];
+      data[smallest] = temp;
     }
   }
 }
