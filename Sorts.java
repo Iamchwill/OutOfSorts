1886b7b49f5b6b338dca3ba3e72d1ddceb657b14
iamchwill <wchen20@stuy.edu>
2020-12-21 15:48:45 -0500

    Made insertionSort() Method

--- Sorts.java
+++ Sorts.java
@@ -1,5 +1,4 @@
 public class Sorts{
-
   public static void bubbleSort(int[] data){
     for (int i = data.length - 1; i > 0; i--){
       boolean done = true;
@@ -24,6 +23,18 @@ public class Sorts{
       int temp = data[i];
       data[i] = data[smallest];
       data[smallest] = temp;
+    }
+  }
+
+  public static void insertionSort(int[] data){
+    for (int i = 1; i < data.length; i++){
+      int change = data[i];
+      int j = i - 1;
+      while (j >= 0 && data[j] > change){
+        data[j + 1] = data[j];
+        j--;
+      }
+      data[j + 1] = change;
     }
   }
 }
